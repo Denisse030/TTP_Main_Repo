044b38956cfd0bc2795d2150886ff63de236f0dd
Denisse <96150584+Denisse030@users.noreply.github.com>
2023-07-05 16:42:14 -0400

    DOM pt.2

--- DOM/scripts/index.js
+++ DOM/scripts/index.js
@@ -50,30 +50,104 @@
 
 
 //querySelector() - Example 1
-const element = document.
-querySelector("div");
-console.log(element);
-element.textContent = "New text";
+// const element = document.
+// querySelector("div");
+// console.log(element);
+// element.textContent = "New text";
 
 //Example 2
-const element2 = document.
-querySelector("#myElement");
-element2.style.backgroundColor = "pink";
+// const element2 = document.
+// querySelector("#myElement");
+// element2.style.backgroundColor = "pink";
 
 //Example 3
-const element3 = document.
-querySelector(".myClass");
-element3.style.fontSize = "100px";
+// const element3 = document.
+// querySelector(".myClass");
+// element3.style.fontSize = "100px";
 
 //Example 4
 // const image = document.
-// querySelector("img");
-// image.setAttribute("src","cat.JPG");
+// querySelector(".myImg");
+// image.setAttribute("src","images/cat.JPG");
 
 //Example 6
-function getValue() {
-    const input = document.
-    querySelector("#myInput");
-    const value = input.value;
-    console.log(value);
+// function getValue() {
+//     const input = document.
+//     querySelector("#myInput");
+//     const value = input.value;
+//     console.log(value);
+// }
+
+
+
+
+
+//Example 1
+// function highlightItems() {
+//     const listItems = document.
+//     querySelector("#myList li")
+//     listItems.forEach(function(item) {
+//         item.style.backgroundColor = "yellow";
+//     } );
+// }
+// highlightItems();
+
+// function printText() {
+//     console.log("clicking button");
+// }
+// function disableButtons() {
+//     const buttons = document.querySelectorAll(".containerbutton");
+//     console.log(buttons);
+//     buttons.forEach(function(b) {
+//         b.disabled = true;
+//     });
+// }
+
+// let randomNum = Math.trunc(Math.random()*6) + 1;
+// function changeMsg(text) {
+//     document.querySelector(".par").textContent = text;
+// }
+
+// function check() {
+//     let inputFromUser = document.querySelector(".numInput").value;
+//     if (!inputFromUser) {
+//     changeMsg("Please Enter a valid number");
+//     }
+//     if (!inputFromUser === randomNum) {
+//         changeMsg("You win!");
+//         document.querySelector("body").style.backgroundColor = "green";
+//     } else {
+//         changeMsg("Try again!");
+//     }
+// }
+
+
+
+
+
+let newElement = document.createElement("p");
+console.log(newElement);
+newElement.textContent = "I just created this";
+
+document.body.appendChild(newElement);
+
+//Ex 2
+const div = document.createElement("div");
+div.classList.add("box");
+console.log(div);
+document.getElementById
+("container").appendChild(div);
+
+
+
+// Ex 5
+function removeMultipleChildElements() {
+    const container = document.getElementById("container4");
+    console.log(container);
+    const paragraphs = container.querySelectorAll("p");
+    paragraphs.forEach(function(paragraph) {
+        container.removeChild(paragraph);
+    })
 }
+
+removeMultipleChildElements();
